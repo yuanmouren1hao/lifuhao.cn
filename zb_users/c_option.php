e3d3d7833fbccab1b7844bf56e69c86c0b2841a8
yuanmouren1hao <342834599@qq.com>
2016-04-05 09:16:31 +0800

    add new project

---
+++ zb_users/c_option.php
@@ -0,0 +1,26 @@
+<?php
+return array (
+  'ZC_SITE_TURNOFF' => false,
+  'ZC_DATABASE_TYPE' => 'mysqli',
+  'ZC_SQLITE_NAME' => '',
+  'ZC_SQLITE_PRE' => 'zbp_',
+  'ZC_MYSQL_SERVER' => 'localhost',
+  'ZC_MYSQL_USERNAME' => 'aoplee',
+  'ZC_MYSQL_PASSWORD' => 'aoplee',
+  'ZC_MYSQL_NAME' => 'lifuhaoblog',
+  'ZC_MYSQL_CHARSET' => 'utf8',
+  'ZC_MYSQL_PRE' => 'zbp_',
+  'ZC_MYSQL_ENGINE' => 'MyISAM',
+  'ZC_MYSQL_PORT' => '3306',
+  'ZC_MYSQL_PERSISTENT' => false,
+  'ZC_PGSQL_SERVER' => 'localhost',
+  'ZC_PGSQL_USERNAME' => 'postgres',
+  'ZC_PGSQL_PASSWORD' => '',
+  'ZC_PGSQL_NAME' => '',
+  'ZC_PGSQL_CHARSET' => 'utf8',
+  'ZC_PGSQL_PRE' => 'zbp_',
+  'ZC_PGSQL_PORT' => '5432',
+  'ZC_PGSQL_PERSISTENT' => false,
+  'ZC_YUN_SITE' => '',
+)
+?>
